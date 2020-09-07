46148e6eba1939a8c7b7e212c7fc9006250ad5fb
CodeClan Student <codeclanstudent@MacBook-Pro-2.local>
2020-09-07 13:19:13 +0100

    First commit

---
+++ src/student.py
@@ -0,0 +1,20 @@
+
+class Student:
+    def __init__(self, input_name, input_cohort):
+        self.name = input_name
+        self.cohort = input_cohort
+
+    
+        
+    def update_name(self, new_name):
+        self.name = new_name
+
+    def change_cohort(self, new_cohort):
+        self.cohort = new_cohort
+
+    def talk(self):
+        return "I can talk!"
+    
+    def say_favourite_language(self, language):
+        return "I love " + language
+
